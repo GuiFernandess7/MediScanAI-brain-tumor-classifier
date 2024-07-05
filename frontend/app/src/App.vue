4d09debb483324f93cdac7f9d80808a79f33c66a
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-05 16:07:50 -0300

    feat: Raw login page added

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <SignUp/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUp,
   }
 }
 </script>
---
+++ frontend/app/src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <img class="logo" src="../assets/MediscanCor.png" alt="">
+    <h1>Sign up</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter Full Name" />
+        <input crm="text" placeholder="Enter CRM" />
+        <input crm="text" placeholder="Enter Email" />
+        <input crm="password" placeholder="Enter Password" />
+        <button>Sign Up</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+.logo {
+    width: 105px;
+}
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
