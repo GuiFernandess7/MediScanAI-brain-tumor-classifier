ad02486e7512b44741912cf40c0e3f38de1df612
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-08 13:54:49 -0300

    config: Axios new configuration

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,8 +1,11 @@
 import App from "./App.vue";
 import router from "./router";
 import { createApp } from "vue";
+import axios from "axios";
 
 const app = createApp(App);
+
+axios.defaults.baseURL = "http://localhost:8000";
 
 app.use(router);
 app.mount("#app");
